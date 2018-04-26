09167a73c348d3f725c6c45b41676daabc171555
Pedro Cunial <pedrocc4@al.insper.edu.br>
2018-04-26 14:16:59 -0300

    pole example

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import gym
 
 
 # could use the non-ram version, but this one is "tinier"
-ROM = 'VideoPinball-ram-v0'
+PINBALL = 'VirtualPinball-ram-v0'
+ROM = 'CartPole-v0'
 
 if __name__ == '__main__':
     env = gym.make(ROM)
-    env.reset()
-    for _ in range(1000):
-        env.render()
-        env.step(env.action_space.sample())
+    highscore = 0
+    for i_episode in range(20):
+        obs = env.reset()
+        points = 0
+        for t in range(1000):
+            env.render()
+            # if angle is pos, move right else left
+            action = 1 if obs[2] > 0 else 0
+            obs, rwd, done, info = env.step(action)
+            points += rwd
+            if done:
+                if points > highscore:
+                    highscore = points
+                break
